5cde003d6c25e819ea5992092e1fe95a73ffa161
gunee7 <gunee7@naver.com>
2018-01-03 00:04:06 +0900

    a

--- Lecher/Android/Work/part2.adapterview/ad23-01.ListViewSample/app/src/main/java/com/example/listviewsample/MainActivity.java
+++ Lecher/Android/Work/part2.adapterview/ad23-01.ListViewSample/app/src/main/java/com/example/listviewsample/MainActivity.java
@@ -19,9 +19,6 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private Button btn1;
     private ListView listv1;
-    private String[] items = {"1", "2", "3", "4", "5"};
-    private String[] months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-    private String[] itemsLV = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +31,9 @@ public class MainActivity extends AppCompatActivity {
         listv1 = findViewById(R.id.listv1);
 
         // adapter 생성
-        ArrayAdapter<String> adapterTextView = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, items);
+        ArrayAdapter<String> adapterTextView = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, (List<String>) textv1);
+
+
 
 
         // spinner와 adapter 연결
@@ -46,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
                 // position : 현재 선택된 배열의 방번호.
-
-                String selecteditem = items[position];
-                textv1.setText(selecteditem);
+                String [] arrays = getResources().getStringArray(R.array.items);
+                mADAPTER
+                textv1.setText();
 
             }
 
@@ -57,77 +56,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-        // adapter 생성
-        ArrayAdapter<String> adapterTextView1 = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, items);
-
-
-        // 3. 위젯 설정정
-        btn1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // 1. edtName에서 값 가져오기
-                String str = editText.getText().toString();
-
-                // 2. txtMsg 에 값을 설정하기.
-                textv1.setText(str);
-
-            }
-        });
-
-
-        /*
-        AutoCompleteTextView 와 adapter연동.
-        1.데이터 만들기
-        2.adapter생성
-        3.AutoCompleteTextView와 adapter연결
-        4.리스너 설정: AutoCompleteTextView에는 필요없음.
-         */
-
-
-        ArrayAdapter<String> adapterMonth = new ArrayAdapter<String>(MainActivity.this
-                , android.R.layout.simple_list_item_1
-                , months);
-        listv1.setAdapter(adapterMonth);
-        listv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // position : 현재 선택된 배열의 방번호.
-                String selecteditem = ((TextView)view).getText().toString();
-                textv1.setText(selecteditem);
-            }
-        });
-
-
-
- /*
-        ListView 와 ArrayAdapter연동.
-        1.데이터 만들기
-        2.adapter생성
-        3.AutoCompleteTextView와 adapter연결
-        4.리스너 설정: AutoCompleteTextView에는 필요없음.
-         */
-
-
-
-        ArrayAdapter<String> adapterLV = new ArrayAdapter<String>(MainActivity.this
-                ,android.R.layout.simple_list_item_1
-                ,itemsLV);
-        listv1.setAdapter( adapterLV);
-        listv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // position : 현재 선택된 배열의 방번호.
-                String selecteditem = ((TextView)view).getText().toString();
-                textv1.setText(selecteditem);
-
-            }
-        });
-
-
     }
 }
